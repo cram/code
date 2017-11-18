977f5ae0a68a7ae8b7e74a87ff795f04396c4f93
Tim Menzies <tim.menzies@gmail.com>
2017-11-18 12:45:24 -0500

    updated

--- etc/htmladd.sh
+++ etc/htmladd.sh
@@ -1,3 +1,4 @@
+echo "patching $1"
 
 stem=$(basename $1 '.html')
 org=lispth
@@ -5,7 +6,15 @@ org=lispth
 icon='https://raw.githubusercontent.com/lispth/code/master/etc/img/lambda.ico'
 img='https://avatars1.githubusercontent.com/u/33398802?s=200&v=4'
 
-_docheader() { cat<<EOF
+_docheader() { 
+
+name="LISPTH: $1"
+
+if [[ "$1" == "index" ]]; then
+  name="LISPTH: Data Mining in Lisp"
+fi
+
+cat<<EOF
  <a href="https://github.com/$org/${org}.github.io"><img z-index: 1
 style="position: absolute; top: 0; right: 0; border: 0;" 
 src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" 
@@ -18,13 +27,15 @@ data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblu
 <a href="https://${org}.github.io/LICENSE">license</a> 
 </p>
 <h1> 
-LISPTH: $1
+$name
 </h1>
+<hr>
 EOF
+
 }
 
 str=$(_docheader $stem)
-sed -i'.bak'  "s?</head>?<link rel=\"shortcut icon\" type=\"image/x-icon\" href="$icon"></head>?1"  $1
-gawk '/<h1>/ {print str; next} /<.h1>/ {next} {print}' str="$str"  $1 > /tmp/$$
-mv /tmp/$$  $1
+sed "s?</head>?<link rel=\"shortcut icon\" type=\"image/x-icon\" href="$icon"></head>?"  $1 > /tmp/$$
+gawk '/<h1>/ {print str; next} /<.h1>/ {next} {print}' str="$str"  /tmp/$$ > /tmp/a$$
+mv /tmp/a$$  $1
      
---
+++ src/index.lisp
@@ -0,0 +1,19 @@
+; 
+; <a href="http://doi.org/10.5281/zenodo.1054269"><img src=https://zenodo.org/badge/DOI/10.5281/zenodo.1054269.svg></a>
+;
+; One of my more effective tricks for teaching data mining (to graduate students) is to:
+;
+; 1. Give them a reference system in a language they do not know, 
+; 2. Then make them port that code to a language that they do (e.g. Python).
+;
+; By the time that is done:
+;
+; - Students have a deep understanding of the core principles invloved;
+; - Students know many of engineering decisions that might need adjusting when the code is used for real application;
+; - Students have the skills required to make, and implement, those decisions.
+;
+; So here is my reference system for my standard data mining toolkit, written in a language
+; that it is **very** unlikely that they have ever used before.
+;
+; Share and enjoy.
+;
