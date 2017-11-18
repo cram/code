7864370b7d3d56c91e081526c127a90bf9866264
Tim Menzies <tim.menzies@gmail.com>
2017-11-18 15:56:29 -0500

    updated

--- src/index.lisp
+++ src/index.lisp
@@ -17,3 +17,82 @@
 ;
 ; Share and enjoy.
 ;
+; ## Installation
+;
+; - Install `sbcl`. Currently I'm using 1.4.2,
+; - Download and unzip https://github.com/lispth/code/archive/master.zip;
+;
+; All the files are now in `src/` and `test/`. Most of the files in `src/X.lisp`
+; have demo code or unit tests in `test/Xok.lisp`. So all files know 
+; where to find its dependents (in either `../src` or `../test`).
+;
+; Edit the file in the root of the distro called `lithp`. Change the following
+; two lines to include the right pathnames.
+;
+;      # begin config
+;      Sbcl="/usr/local/bin/sbcl"
+;      Test="$HOME/gits/lispth/code/test"
+;      # end config here
+;
+; ## Writing New Code
+;
+; **Step1:** Write a file `src/xxx.lisp` file in `src/`. Ensire its first lines are  
+;
+;      (load "../src/lib") 
+;
+; **Step2:** Write a second file `tests/xxxok.lisp'. Ensure its first lines are
+; 
+;      (load "../src/lib")
+;      (reload "../src/xxx")
+;      
+; **IMPORTANTL** Note the `reload` command: this loads a file, but only if it has not been used before.  
+; So all your code should start with one `load` command then some `reload` commands. 
+;
+; **Step3:** Into `tests/xxxok.lisp`, add multiple `deftest` commands. These functions should have a
+; a documentation string, then arbitrary LISP code. Any call `test` will iincrement
+; counters of how many times those tests _pass_ or _fail_.
+;
+;    (deftest r2! ()
+;       "does 'r1' round to 2 decimal places"
+;       (test 0.13 (r2 0.127456)))
+;
+; **Step4:** Run the above. Create an `ok` file
+; 
+;     #!/bin/bash
+;     ok () {
+;         local here=$PWD;
+;         cd $Here/test;
+;         echo ";;;; ../test/${1}ok";
+;         time $Sbcl --noinform --disable-debugger --eval "
+;                       (progn (reload \"../test/${1}ok\")
+;                              (tests) 
+;                             (exit))"
+;         cd $here
+;     }
+;     ok $1
+;
+;
+; ## Structure
+;
+; There are two parts, _background tools_ and _core_
+;
+; ### Background
+;
+; Defined in `boot.lisp`
+
+(defun relad (file)
+  "Load file.lisp it it has not been loaded before")
+
+(defun def
+;
+
+
+(general
+  (boot  ; start up routines. 
+    (does 
+      deftest
+      test
+      tests
+      )
+(table ; table.lisp
+   (has (rows cols
